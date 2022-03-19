e00cf0e7d22b33bad7ea937f94ef40ad8ffdfccd
Zakhar Kalachev <zackfnx@yandex.ru>
2022-03-19 20:08:21 +0300

    Input-Output via files in Python

--- python/rosalind_fib.py
+++ python/rosalind_fib.py
@@ -1,3 +1,14 @@
+from typing import Tuple
+
+def read_data(filepath: str) -> Tuple[int, int]:
+    with open(filepath, mode='r') as file:
+        line = file.read().rstrip('\n')
+        return tuple(map(int, line.split()))
+
+def write_data(filepath: str, data: int) -> None:
+    with open(filepath, mode='w') as file:
+        file.write(str(data))
+
 def count_rabbits_pairs(months: int, litter: int) -> int:
     if months <= 2:
         return 1
@@ -5,6 +16,6 @@ def count_rabbits_pairs(months: int, litter: int) -> int:
         return count_rabbits_pairs(months - 1, litter) + litter * count_rabbits_pairs(months - 2, litter)
 
 if __name__ == '__main__':
-    dataset = [31, 2]
-    result = count_rabbits_pairs(*dataset)
-    print(result)
+    data = read_data('input/rosalind_fib.txt')
+    result = count_rabbits_pairs(*data)
+    write_data('output/rosalind_fib.txt', result)
--- python/rosalind_iprb.py
+++ python/rosalind_iprb.py
@@ -1,3 +1,15 @@
+from asyncore import read
+from typing import Tuple
+
+def read_data(filepath: str) -> Tuple[int, int, int]:
+    with open(filepath, mode='r') as file:
+        line = file.read().rstrip('\n')
+        return tuple(map(int, line.split()))
+
+def write_data(filepath: str, data: float) -> None:
+    with open(filepath, mode='w') as file:
+        file.write(str(data))
+
 def dominant_allele_probability(homozygous_dominant: int, heterozygous_recessive: int, homozygous_recessive: int) -> float:
     k = homozygous_dominant
     m = heterozygous_recessive
@@ -21,6 +33,6 @@ def dominant_allele_probability(homozygous_dominant: int, heterozygous_recessive
     return 1 - (c1 + c2 + c3)
 
 if __name__ == '__main__':
-    dataset = [30, 22, 15]
-    result = dominant_allele_probability(*dataset)
-    print(result)
+    data = read_data('input/rosalind_iprb.txt')
+    result = dominant_allele_probability(*data)
+    write_data('output/rosalind_iprb.txt', result)
